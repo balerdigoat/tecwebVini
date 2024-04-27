e620cf847d44a9a15254ee6a53358835c771b9a2
BalerdiGoat <167622430+balerdigoat@users.noreply.github.com>
2024-04-27 10:25:14 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,12 @@
+
+    function tabuada() {
+    var numero = document.getElementById("numero").value;
+    var tabuada = document.getElementById("tabuada");
+
+    for (var i = 1; i <= 10; i++) {
+        var resultado = numero * i;
+    console.log(resultado);
+    
+    }
+
+ }
